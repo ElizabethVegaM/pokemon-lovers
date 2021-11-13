dd3d209e966bd2b7501ad3bbb7af5a6b5096e487
Elizabeth Vega <eb.vega.m@gmail.com>
2021-11-13 00:33:14 -0300

    (feat): draws cards for each pokemon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { example } from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
+const pokeArr = data.pokemon;
 console.log(example, data);
+const container = document.querySelector("#cardContainer");
+
+const drawCard = (pokemon) => {
+  return `<li>
+    <figure>
+      <img src="${pokemon.img}" alt="${pokemon.name} picture">
+    </figure>
+    <div class="pokemon-info">
+      <p>#${pokemon.num}</p>
+      <h5>${pokemon.name}</h5>
+      <div>
+        ${pokemon.type.map(type => `<span class="${type}-btn">${type}<span>`)}
+      </div>
+    </div>
+  </li>`;
+};
+
+window.addEventListener("load", () => {
+  pokeArr.map((pokemon) => {
+    container.innerHTML += drawCard(pokemon);
+  });
+});
