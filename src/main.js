5148fcec586c2a514bf424350e7e723af84824d9
Elizabeth Vega <eb.vega.m@gmail.com>
2021-11-14 20:32:19 -0300

    (styles): adds styles to extended card information

--- src/main.js
+++ src/main.js
@@ -6,56 +6,73 @@ let filters = [];
 console.log(data);
 
 const container = document.querySelector("#cardContainer");
-const filterBtn = document.querySelector('#filter');
-const orderBtn = document.querySelector('#order');
-
+const filterBtn = document.querySelector("#filter");
+const orderBtn = document.querySelector("#order");
 
 const drawExtendedCard = (pokemon) => {
-  const card = document.createElement('article');
+  const card = document.createElement("article");
+  card.className = "extended-card";
 
-  card.innerHTML = `<article>
-    <h2>${pokemon.name}</h2>
-    <span>#${pokemon.num}</span>
-    <section>
-      <img src="${pokemon.img}" alt="imagen">
-      <div>
-        <p>${pokemon.about}</p>
-        <div>
-          <ul>
-            <li>
-              <span>Height</span>
-              <span>${pokemon.size.height}</span>
-            </li>
-            <li>
-              <span>Weight</span>
-              <span>${pokemon.size.weight}</span>
-            </li>
-          </ul>
+  card.innerHTML = `<section class="card-title">
+      <h2>${pokemon.name} <span>#${pokemon.num}</span></h2>
+    </section>
+    <section class="card-body">
+      <figure>
+        <img src="${pokemon.img}" alt="imagen">
+      </figure>
+      <div class="card-info">
+        <p class="card-about">${pokemon.about}</p>
+        <div class="details">
+          <p>
+            <span>Height</span>
+            <span>${pokemon.size.height}</span>
+          </p>
+          <p>
+            <span>Weight</span>
+            <span>${pokemon.size.weight}</span>
+          </p>
+          <p>
+            <span>Generation</span>
+            <span>${pokemon.generation.name}</span>
+          </p>
         </div>
       </div>
     </section>
-    <button>Explore More Pokémon</button>
-  </article>`
+    <section class="card-types">
+      <h3>Type</h3>
+      ${pokemon.type.map(
+        (type) => `<span class="type-tag ${type}-tag">${type}<span>`
+      )}
+      <h4>Resistant</h4>
+      ${pokemon.resistant.map(
+        (type) => `<span class="type-tag ${type}-tag">${type}<span>`
+      )}
+      <h4>Weaknesses</h4>
+      ${pokemon.weaknesses.map(
+        (type) => `<span class="type-tag ${type}-tag">${type}<span>`
+      )}
+    </section>
+    <button>Explore More Pokémon</button>`;
 
-  card.querySelector('button').addEventListener('click', () => {
-    container.innerHTML = '';
+  card.querySelector("button").addEventListener("click", () => {
+    container.innerHTML = "";
     pokeArr.map((data) => {
       container.appendChild(drawCard(data));
     });
-  })
+  });
 
   return card;
 };
 
 const drawCard = (pokemon) => {
   const card = document.createElement("li");
-  card.className = "pokemonCard";
+  card.className = "basic-card";
 
   card.innerHTML = `
   <figure>
     <img src="${pokemon.img}" alt="${pokemon.name} picture">
   </figure>
-  <div class="pokemon-info">
+  <section>
     <p>#${pokemon.num}</p>
     <h5>${pokemon.name}</h5>
     <div class="abilities">
@@ -63,10 +80,10 @@ const drawCard = (pokemon) => {
         (type) => `<span class="type-tag ${type}-tag">${type}<span>`
       )}
     </div>
-  </div>`;
+  </section>`;
 
   card.addEventListener("click", () => {
-    container.innerHTML = '';
+    container.innerHTML = "";
     container.appendChild(drawExtendedCard(pokemon));
   });
 
@@ -79,21 +96,22 @@ window.addEventListener("load", () => {
     container.appendChild(drawCard(pokemon));
   });
   filters = reduceData(filters.flat());
-  filters.map(type => filterBtn.innerHTML += `<option value=${type}>${type}</option>`);
+  filters.map(
+    (type) => (filterBtn.innerHTML += `<option value=${type}>${type}</option>`)
+  );
 });
 
-
-filterBtn.addEventListener('change', (e) => {
+filterBtn.addEventListener("change", (e) => {
   const newData = filterData(pokeArr, e.target.value);
-  container.innerHTML = '';
-  newData.map(pokemon => container.appendChild(drawCard(pokemon)));
-})
+  container.innerHTML = "";
+  newData.map((pokemon) => container.appendChild(drawCard(pokemon)));
+});
 
-orderBtn.addEventListener('change', (e) => {
+orderBtn.addEventListener("change", (e) => {
   let sortBy = e.target.value;
-  const newData = ['low', 'high'].includes(sortBy) ? sortData(pokeArr, 'num', sortBy) : sortData(pokeArr, 'name', sortBy);
-  container.innerHTML = '';
-  newData.map(pokemon => container.appendChild(drawCard(pokemon)));
-})
-
-
+  const newData = ["low", "high"].includes(sortBy)
+    ? sortData(pokeArr, "num", sortBy)
+    : sortData(pokeArr, "name", sortBy);
+  container.innerHTML = "";
+  newData.map((pokemon) => container.appendChild(drawCard(pokemon)));
+});
