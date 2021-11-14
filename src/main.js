4f738f8d6e4c6455be63c1ee94740d18cf51940f
Elizabeth Vega <eb.vega.m@gmail.com>
2021-11-14 17:08:31 -0300

    (styles): details on pokemon cards

--- src/main.js
+++ src/main.js
@@ -5,23 +5,67 @@ const pokeArr = data.pokemon;
 console.log(example, data);
 const container = document.querySelector("#cardContainer");
 
-const drawCard = (pokemon) => {
-  return `<li>
-    <figure>
-      <img src="${pokemon.img}" alt="${pokemon.name} picture">
-    </figure>
-    <div class="pokemon-info">
-      <p>#${pokemon.num}</p>
-      <h5>${pokemon.name}</h5>
+const drawExtendedCard = (pokemon) => {
+  const container = document.createElement('article');
+
+  container.innerHTML = `<article>
+    <h2>${pokemon.name}</h2>
+    <span>#${pokemon.num}</span>
+    <section>
+      <img src="${pokemon.img}" alt="imagen">
       <div>
-        ${pokemon.type.map(type => `<span class="${type}-btn">${type}<span>`)}
+        <p>${pokemon.about}</p>
+        <div>
+          <ul>
+            <li>
+              <span>Height</span>
+              <span>${pokemon.size.height}</span>
+            </li>
+            <li>
+              <span>Weight</span>
+              <span>${pokemon.size.weight}</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </li>`;
+    </section>
+    <button>Explore More Pokémon</button>
+  </article>`
+
+return container;
 };
+
+const drawCard = (pokemon) => {
+  const card = document.createElement("li");
+  card.className = "pokemonCard";
+
+  card.innerHTML = `
+  <figure>
+    <img src="${pokemon.img}" alt="${pokemon.name} picture">
+  </figure>
+  <div class="pokemon-info">
+    <p>#${pokemon.num}</p>
+    <h5>${pokemon.name}</h5>
+    <div class="abilities">
+      ${pokemon.type.map(
+        (type) => `<span class="type-tag ${type}-tag">${type}<span>`
+      )}
+    </div>
+  </div>`;
+
+  card.addEventListener("click", () => {
+    console.log(pokemon);
+    container.innerHTML = '';
+    container.appendChild(drawExtendedCard(pokemon));
+  });
+
+  return card;
+};
+
+
 
 window.addEventListener("load", () => {
   pokeArr.map((pokemon) => {
-    container.innerHTML += drawCard(pokemon);
+    container.appendChild(drawCard(pokemon));
   });
 });
