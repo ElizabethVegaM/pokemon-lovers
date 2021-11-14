190dbb9dd5a557e6dd920e4f76c5fe3e064ea21a
Elizabeth Vega <eb.vega.m@gmail.com>
2021-11-14 19:08:04 -0300

    (test): adds tests for data.js functions

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,95 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterData, reduceData, sortData } from "../src/data.js";
 
+const exampleArr = [
+  {
+    num: "001",
+    name: "bulbasaur",
+    type: ["grass", "poison"],
+  },
+  {
+    num: "057",
+    name: "primeape",
+    type: ["fighting"],
+  },
+  {
+    num: "086",
+    name: "seel",
+    type: ["water"],
+  },
+  {
+    num: "162",
+    name: "furret",
+    type: ["normal"],
+  },
+];
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("reduceData", () => {
+  const testArr = [0, 4, 3, 4, 1, 5, 2, 3];
+
+  it("is a function", () => {
+    expect(typeof reduceData).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("returns an array with no duplicated elements", () => {
+    expect(reduceData(testArr)).toEqual([0, 4, 3, 1, 5, 2]);
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+describe("filterData", () => {
+  it("is a function", () => {
+    expect(typeof filterData).toBe("function");
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it("returns the same array when 'all' condition is given", () => {
+    expect(filterData(exampleArr, "all")).toEqual(exampleArr);
+  });
+
+  it("returns an array with only 'fighting' type pokemones", () => {
+    expect(filterData(exampleArr, "fighting")).toEqual([
+      {
+        num: "057",
+        name: "primeape",
+        type: ["fighting"],
+      },
+    ]);
+  });
+});
+
+describe("sortData", () => {
+  it("is a function", () => {
+    expect(typeof sortData).toBe("function");
+  });
+
+  it("returns the same array when 'num' and 'asc' conditions are given", () => {
+    expect(sortData(exampleArr, "num", "asc")).toEqual(exampleArr);
+  });
+
+  it("returns an array with reversed elements when 'des' condition is given", () => {
+    expect(sortData(exampleArr, "num", "des")).toEqual(exampleArr.reverse());
+  });
+
+  it("returns an array with ordered elements by name when 'name' and 'asc' conditions are given", () => {
+    expect(sortData(exampleArr, "name", "asc")).toEqual([
+      {
+        num: "001",
+        name: "bulbasaur",
+        type: ["grass", "poison"],
+      },
+      {
+        num: "162",
+        name: "furret",
+        type: ["normal"],
+      },
+      {
+        num: "057",
+        name: "primeape",
+        type: ["fighting"],
+      },
+      {
+        num: "086",
+        name: "seel",
+        type: ["water"],
+      },
+    ]);
   });
 });
